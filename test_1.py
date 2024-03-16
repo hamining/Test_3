485b59569f066dcf54c8fe16df4c7f4e3d85511e
hamin <0201061@naver.com>
2024-03-16 13:42:42 +0900

    main

--- test_1.py
+++ test_1.py
@@ -3,3 +3,31 @@ print("hello_2")
 
 import pandas as pd 
 import streamlit as st
+
+st.set_page_config(page_title="Streamlit_TEST")
+st.title('Python 공부')
+
+st.write("파이써으로 웹사이트가 뚝닥?")
+
+# Title of the app
+st.title('Excel/CSV File Data Viewer')
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose an Excel or CSV file", type=['csv', 'xlsx'])
+
+if uploaded_file is not None:
+    try:
+        if uploaded_file.type == "text/csv":
+            # For CSV files
+            df = pd.read_csv(uploaded_file)
+        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            # For Excel files
+            df = pd.read_excel(uploaded_file)
+
+        # Display the first 5 rows of the DataFrame
+        st.dataframe(df.head())
+
+    except Exception as e:
+        st.error(f"Error: {e}")
+else:
+    st.info('Upload a file to display the data')
